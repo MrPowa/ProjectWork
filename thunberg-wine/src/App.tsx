a1c5206a68a6593df08f65820549d160681c9da8
DaniloBattaglia <battagliadanilovincenzo@gmail.com>
2024-07-17 15:51:40 +0200

    last code

--- thunberg-wine/src/App.tsx
+++ thunberg-wine/src/App.tsx
@@ -1,16 +1,23 @@
 import './App.css';
-import {BrowserRouter} from "react-router-dom";
-import"./App.css";
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
 import MainRoutes from "./Routes/mainRoutes";
+import RoundedButton from './Components/RoundedBotton';
+import { CartProvider } from './context/CartContext';
+import { Header } from './Components/Header';
 
-
-function App(){
+function App() {
   // return <GridCard />
   return (
-    <BrowserRouter>
-    <MainRoutes />
-    </BrowserRouter>
+    <div className='App'>
+      <CartProvider>
+        <BrowserRouter>
+          <Header />
+          <MainRoutes />
+        </BrowserRouter>
+      </CartProvider>
+    </div>
   )
 }
 
-export default App;
+export default App;
